fff84c12de6d410147f9d62cec32fbf996a70d7e
Nelson Roza Junior <jr.1403@hotmail.com>
2020-10-12 12:51:57 -0300

    Owncloud compose

---
+++ compose/owncloud/home/config/config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocwt596am2ul',
+  'passwordsalt' => 'xzRgcEg/ullUGI6uKFTKbNUjkFqdGV',
+  'secret' => 'Lqzw6glaIrpeHktg/YZlUda1k4+lhdhSfaVdAGBqUk33j43o',
+  'trusted_domains' => 
+  array (
+    0 => 'dockerhost:9010',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'http://dockerhost:9010',
+  'dbtype' => 'mysql',
+  'version' => '10.0.10.4',
+  'dbname' => 'OwnCloudDb',
+  'dbhost' => 'db',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'oc_admin',
+  'dbpassword' => 'EkNhxKaPx5eNd1FA5Q0Q1q5H65gGDv',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
